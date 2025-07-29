cc583b5042be31055d964d2b859bde92fb9f4b54
xuxianchen <15172925+xuxianchen@user.noreply.gitee.com>
2025-07-29 22:34:10 +0800

    20250729

--- stream-py/MarkItDown/word_transition_md.py
+++ stream-py/MarkItDown/word_transition_md.py
@@ -3,7 +3,7 @@ from markitdown import MarkItDown
 # 创建一个 MarkItDown 实例
 
 md = MarkItDown()
-result = md.convert(r"E:\实训\专高六\cdh\Linux安装 MySQL.pdf")
+result = md.convert(r"C:\Users\55316\Desktop\seatunnel 配置文件大全.pdf")
 markdown_content = result.text_content
 markdown_text = result.markdown
 print(markdown_content)
