20ba86e9001660be68e5952b9c065f440e617758
xuxianchen <15172925+xuxianchen@user.noreply.gitee.com>
2025-08-14 20:56:15 +0800

    20250814

--- stream-py/MarkItDown/word_transition_md.py
+++
@@ -1,9 +0,0 @@
-from markitdown import MarkItDown
-
-# 创建一个 MarkItDown 实例
-
-md = MarkItDown()
-result = md.convert(r"C:\Users\55316\Desktop\seatunnel 配置文件大全.pdf")
-markdown_content = result.text_content
-markdown_text = result.markdown
-print(markdown_content)
